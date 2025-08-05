9dcddcb7bdc83029e8b3b1ab2a24ed75d47a559a
Phichaiyut <pichayutkingpila@gmail.com>
2025-08-05 19:22:38 +0700

    Add MySQL integration and body-parser to server
    Added mysql2 and body-parser dependencies. Implemented a new /testdb endpoint in server.js to fetch users from the MySQL 'camp' database. This enables database connectivity for future backend features.

--- server.js
+++ server.js
@@ -1,11 +1,37 @@
 import express from "express";
 import cors from "cors";
+import mysql from "mysql2/promise";
+import bodyParser from "body-parser";
+
 const app = express();
+
 const corsOption = {
   origin: ["http://localhost:5173"],
 };
 
 app.use(cors(corsOption));
+
+app.get("/testdb", (req, res) => {
+  mysql
+    .createConnection({
+      host: "localhost",
+      user: "root",
+      password: "root",
+      database: "camp",
+    })
+    .then((conn) => {
+      //promise
+      conn
+        .query("SELECT * FROM users")
+        .then((result) => {
+          res.json(result[0]);
+        })
+        .catch((error) => {
+          console.error("Error fetching users:", error.message);
+          res.status(500).json({ error: "Error fetching users" });
+        });
+    });
+});
 
 app.get("/api", (req, res) => {
   res.json({ car: ["mercedes", "BMV", "Tesla"] });
