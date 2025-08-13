6bbd39ca77363fa0c1680d9c5a1c82c02613180a
Phichaiyut <pichayutkingpila@gmail.com>
2025-08-13 19:54:15 +0700

    Add CRUD endpoints for users resource
    Implemented GET, POST, PUT, and DELETE endpoints for managing users. These endpoints allow fetching all users, fetching a user by ID, updating a user by ID, and deleting a user by ID, with appropriate error handling.

--- server.js
+++ server.js
@@ -22,11 +22,12 @@ const initMysql = async () => {
   });
 };
 
+//GET /user สำหรับดึง user ทั้งหมเ
 app.get("/users", async (req, res) => {
   const result = await conn.query("SELECT * FROM users");
   res.json(result[0]);
 });
-
+// POST /users สำหรับเพิ่มข้อมูลไปยัง db
 app.post("/users", async (req, res) => {
   try {
     let user = req.body;
@@ -41,6 +42,77 @@ app.post("/users", async (req, res) => {
 
     res.status(500).json({
       message: "someting wrong",
+    });
+  }
+});
+
+// GET /users/:id สำหรับดึง users รายคนออกมา
+app.get("/users/:id", async (req, res) => {
+  try {
+    let id = req.params.id;
+    const result = await conn.query("SELECT * FROM users WHERE id = ?", id);
+
+    if (result[0].length == 0) {
+      throw { statusCode: 404, message: "หาไม่เจอ" };
+    }
+    res.json(result[0][0]);
+  } catch (error) {
+    console.log("error message", error.message);
+    let statusCode = error.statusCode || 500;
+    res.status(statusCode).json({
+      message: "someting wrong",
+      errorMessage: error.message,
+    });
+  }
+});
+
+// PUT /users/:id สำหรับการแก้ไข user รายคน (ตาม id)
+app.put("/users/:id", async (req, res) => {
+  try {
+    let id = req.params.id;
+    let updateUser = req.body;
+
+    const result = await conn.query("UPDATE users SET ? WHERE id = ?", [
+      updateUser,
+      id,
+    ]);
+
+    if (result[0].affectedRows === 0) {
+      throw { statusCode: 404, message: "หาไม่เจอ" };
+    }
+    res.json({
+      message: "update ok",
+      data: result[0],
+    });
+  } catch (error) {
+    console.log("error message", error.message);
+    const statusCode = error.statusCode || 500;
+    res.status(statusCode).json({
+      message: "someting wrong",
+      errorMessage: error.message,
+    });
+  }
+});
+
+// DELETE /users/:id  สำหรับการลบข้อมูล user รายคน (ตาม id)
+app.delete("/users/:id", async (req, res) => {
+  try {
+    let id = req.params.id;
+    const result = await conn.query("DELETE From users WHERE id = ?", id);
+
+    if (result[0].affectedRows === 0) {
+      throw { statusCode: 404, message: "หาไม่เจอ" };
+    }
+    res.json({
+      message: "delete ok",
+      data: result[0],
+    });
+  } catch (error) {
+    console.log("error message", error.message);
+    const statusCode = error.statusCode || 500;
+    res.status(statusCode).json({
+      message: "someting wrong",
+      errorMessage: error.message,
     });
   }
 });
